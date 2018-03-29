ca6af4f42a17c9baf8ca709b7857c0ff09ad201d
Kelaiya <kelaiyarao1@gmail.com>
2018-03-29 18:40:12 -0400

    added server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+module.exports = app;
+const bodyParser = require('body-parser');
+
+// body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static file-serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.use(morgan('dev'));
+
+// Sends index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+})
+
+// Error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+// Start listening 
+app.listen(process.env.PORT || 8080, () => console.log('Mixing it up on port 8080'))
